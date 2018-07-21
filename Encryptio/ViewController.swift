2812390b8a14edeee7d29c9cce66c860b840e15e
AnumQ <anumqudsia@hotmail.com>
2018-07-21 20:25:41 +0200

    Initial Commit

---
+++ Encryptio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Encryptio
+//
+//  Created by Anum Qudsia on 21/07/2018.
+//  Copyright © 2018 anum.qudsia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
